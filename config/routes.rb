949e66f0d6458922f1ed616dcaa446c03f71564b
surenot <maxime.clauss@gmail.com>
2015-04-14 07:44:47 +0900

    Layout basics

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-  get 'static_pages/about'
+  get 'about' => 'static_pages#about'
+  get 'signup'  => 'users#new'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
