622722e015e2472cfde5d67253d8d6a34f7f3f45
surenot <maxime.clauss@gmail.com>
2015-04-14 00:31:16 +0900

    About page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'application#hello'
+  # root 'static_pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'static_pages/about'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | surenot"
   end
 
 end
