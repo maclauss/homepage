e74dc9955e8d8e865d6f2ea73fe36cec6e0f9604
surenot <maxime.clauss@gmail.com>
2015-04-20 00:28:37 +0900

    Enhance About design

--- app/helpers/view_helper.rb
+++ app/helpers/view_helper.rb
@@ -1,8 +1,16 @@
 module ViewHelper
 
-  # Logs in the given user.
-  def skill(skill_name, stars)
-    ret = "<div class=\"row\"><div class=\"col-xs-4\">"+skill_name+"</div><div class=\"col-xs-8\">"
+  def skill_superstar(skill_name)
+    ret = "<div class=\"row\"><div class=\"col-xs-8 skill\">"+skill_name+"</div><div class=\"col-xs-4 skill-superstar\">"
+    5.times do
+      ret = ret+"<span class=\"glyphicon glyphicon-star\"></span>"
+    end
+    ret = ret+"</div></div>"
+    ret.html_safe
+  end
+
+  def skill_lg(skill_name, stars)
+    ret = "<div class=\"row\"><div class=\"col-xs-8 skill\">"+skill_name+"</div><div class=\"col-xs-4 skill-stars\">"
     stars.times do
       ret = ret+"<span class=\"glyphicon glyphicon-star\"></span>"
     end
