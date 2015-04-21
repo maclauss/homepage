513611aa1a8c2696c001fde6bf80bf07bb3113a6
Maxime <maxime.clauss@gmail.com>
2015-04-21 09:17:10 +0000

    Add cache helper

--- app/models/ip_address.rb
+++ app/models/ip_address.rb
@@ -1,3 +1,5 @@
 class IpAddress < ActiveRecord::Base
   has_many :connections, dependent: :destroy
+  validates :ip_address, presence: true, length: { minimum: 7,maximum: 15 },
+                    uniqueness: { case_sensitive: true }
 end
